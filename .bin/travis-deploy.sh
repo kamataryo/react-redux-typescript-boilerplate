d3c8cef3becb398ceead18e375a1c7b6c0427dff
KamataRyo <mugil.cephalus+Pagophilla.local@gmail.com>
2017-07-09 17:27:08 +0900

    Add travis deploy

---
+++ .bin/travis-deploy.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+set -e
+
+if [ $TRAVIS_BRANCH != "master" ]; then
+  echo "TRAVIS_BRANCH=$TRAVIS_BRANCH"
+  echo 'Not deploying.'
+  exit 0
+fi
+
+cd dist
+
+git init
+git config user.name 'kamataryo@travis'
+git config user.email "kamataryo@users.noreply.github.com"
+git remote add origin git@github.com:kamataryo/react-redux-typescript-boilerplate.git
+
+git checkout gh-pages
+git add .
+git commit -m "Deploy [made in travis cron]"
+git push origin gh-pages
